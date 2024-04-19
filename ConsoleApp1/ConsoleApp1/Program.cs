418bf95d2820cf2408afc0c84fe14de8a6faaf5a
Promety <dmitrihard0@gmail.com>
2024-04-19 10:11:18 +0300

    Создание класса Plane Заполнение класса Main

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.IO;
+
 
 namespace ConsoleApp1
 {
+    class Plane
+    {
+        public string Model { get; set; } //Название
+        public int MaxFlRange { get; set; } // Максимальная дальность полета
+        public int CruisingSpeed { get; set; } // Крейсерная скорость
+        public void Print()
+        {
+            Console.WriteLine("Введите модель: " + Model);
+            Console.WriteLine("Введите максимальную дальность полёта: " + MaxFlRange);
+            Console.WriteLine("Введите крейсерскую скорость: " + CruisingSpeed);
+
+            using (StreamWriter os = new StreamWriter("result.txt", true))
+            {
+                os.WriteLine(Model + " " + MaxFlRange + " " + CruisingSpeed);
+            }
+        }
+
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int size = 0;
+            Console.WriteLine("Введите Кол-во самолетов (размерность массива):");
+
+            size = int.Parse(Console.ReadLine());
+            Plane[] A = new Plane[size];
+            for (int i = 0; i < size; i++)
+            {
+                A[i] = new Plane();
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Введите название " + (i + 1) + " самолета :");
+                A[i].Model = Console.ReadLine();
+
+                Console.Write("Введите максимальную дальность полёта " + (i + 1) + " самолета :");
+                A[i].MaxFlRange = int.Parse(Console.ReadLine());
+
+                Console.Write("Введите крейсерскую скорость " + (i + 1) + " самолета :");
+                A[i].CruisingSpeed = int.Parse(Console.ReadLine());
+
+
+
+            }
+
+            
+            foreach (var plane in A)
+            {
+                Array.Sort(A, (x, y) => x.MaxFlRange.CompareTo(y.MaxFlRange));
+                Array.Sort(A, (x, y) => x.CruisingSpeed.CompareTo(y.CruisingSpeed));
+                plane.Print();
+            }
         }
+
     }
 }
+    //class PlaneControl
+     //   {
+     //   Plane.Sort();
+       // Array.Sort(A, (x, y) => x..CompareTo(y.S));
+          //  Array.Sort(A, (x, y) => x.kolvo.CompareTo(y.kolvo));
+      //  }
+
