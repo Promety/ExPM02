2da0fcd4240314c7bb758eb24e2fe36ef8d1b37f
Promety <dmitrihard0@gmail.com>
2024-04-19 11:02:34 +0300

    Исправление кода на List и добавление сортировки

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,71 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 
 namespace ConsoleApp1
 {
-    class Plane
+    public class Plane
     {
-        public string Model { get; set; } //Название
-        public int MaxFlRange { get; set; } // Максимальная дальность полета
-        public int CruisingSpeed { get; set; } // Крейсерная скорость
-        public void Print()
-        {
-            Console.WriteLine("Введите модель: " + Model);
-            Console.WriteLine("Введите максимальную дальность полёта: " + MaxFlRange);
-            Console.WriteLine("Введите крейсерскую скорость: " + CruisingSpeed);
+        public string Model { get; set; }
+        public double MaxRange { get; set; }
+        public double CruiseSpeed { get; set; }
 
-            using (StreamWriter os = new StreamWriter("result.txt", true))
-            {
-                os.WriteLine(Model + " " + MaxFlRange + " " + CruisingSpeed);
-            }
+        public Plane(string model, double maxRange, double cruiseSpeed)
+        {
+            Model = model;
+            MaxRange = maxRange;
+            CruiseSpeed = cruiseSpeed;
+        }
+        
+    }
+
+    public class PlaneControl
+    {
+        private List<Plane> planes = new List<Plane>();
+
+        public void AddPlane(Plane plane)
+        {
+            planes.Add(plane);
         }
 
+        public void SortPlanes()
+        {
+            planes.Sort((x, y) =>
+            {
+                if (x.MaxRange != y.MaxRange)
+                    return y.MaxRange.CompareTo(x.MaxRange);
+                else
+                    return y.CruiseSpeed.CompareTo(x.CruiseSpeed);
+            });
+        }
+
+        public void SaveToFile(string filename)
+        {
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                foreach (Plane plane in planes)
+                {
+                    writer.WriteLine($"Model: {plane.Model}, Max Range: {plane.MaxRange}, Cruise Speed: {plane.CruiseSpeed}");
+                }
+            }
+        }
     }
+
     class Program
     {
         static void Main()
         {
-            int size = 0;
-            Console.WriteLine("Введите Кол-во самолетов (размерность массива):");
+            PlaneControl control = new PlaneControl();
 
-            size = int.Parse(Console.ReadLine());
-            Plane[] A = new Plane[size];
-            for (int i = 0; i < size; i++)
-            {
-                A[i] = new Plane();
-            }
+            Console.Write("Введите кол-во самолетов (размер массива): ");
+            int size = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < size; i++)
             {
-                Console.Write("Введите название " + (i + 1) + " самолета :");
-                A[i].Model = Console.ReadLine();
+                Console.Write("Название модели: ");
+                string model = Console.ReadLine();
 
-                Console.Write("Введите максимальную дальность полёта " + (i + 1) + " самолета :");
-                A[i].MaxFlRange = int.Parse(Console.ReadLine());
+                Console.Write("Максимальная дальность полёта: ");
+                double maxRange = double.Parse(Console.ReadLine());
 
-                Console.Write("Введите крейсерскую скорость " + (i + 1) + " самолета :");
-                A[i].CruisingSpeed = int.Parse(Console.ReadLine());
+                Console.Write("Крейсерская скорость: ");
+                double cruiseSpeed = double.Parse(Console.ReadLine());
 
-
+                Plane plane = new Plane(model, maxRange, cruiseSpeed);
+                control.AddPlane(plane);
 
             }
 
-            
-            foreach (var plane in A)
-            {
-                Array.Sort(A, (x, y) => x.MaxFlRange.CompareTo(y.MaxFlRange));
-                Array.Sort(A, (x, y) => x.CruisingSpeed.CompareTo(y.CruisingSpeed));
-                plane.Print();
-            }
+            control.SortPlanes();
+            control.SaveToFile("planes.txt");
+
         }
-
     }
+
 }
-    //class PlaneControl
-     //   {
-     //   Plane.Sort();
-       // Array.Sort(A, (x, y) => x..CompareTo(y.S));
-          //  Array.Sort(A, (x, y) => x.kolvo.CompareTo(y.kolvo));
-      //  }
+   
 
